a788fa615f41007ce231dc0ad8dbead04ad96508
sabtain.anwar <zainop001@gmail.com>
2024-09-21 23:36:22 -0700

    implementing chats

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,48 +1,45 @@
-// const Message = require('../models/message.model');
-// const Conversation = require('../models/conversation.model');
+const Message = require('../models/message.model');
+const Conversation = require('../models/conversation.model');
 
-// // Create or get a conversation between two users
-// exports.getConversation = async (req, res) => {
-//     const { userId, friendId } = req.params;
-//     try {
-//         let conversation = await Conversation.findOne({
-//             participants: { $all: [userId, friendId] }
-//         });
+exports.getConversation = async (req, res) => {
+    const { userId, friendId } = req.params;
+    try {
+        let conversation = await Conversation.findOne({
+            participants: { $all: [userId, friendId] }
+        });
 
-//         if (!conversation) {
-//             conversation = new Conversation({ participants: [userId, friendId] });
-//             await conversation.save();
-//         }
+        if (!conversation) {
+            conversation = new Conversation({ participants: [userId, friendId] });
+            await conversation.save();
+        }
 
-//         res.status(200).json(conversation);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// };
+        res.status(200).json(conversation);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
 
-// // Send a message
-// exports.sendMessage = async (req, res) => {
-//     const { conversationId, senderId, text } = req.body;
-//     try {
-//         const message = new Message({
-//             conversationId,
-//             sender: senderId,
-//             text
-//         });
-//         await message.save();
+exports.sendMessage = async (req, res) => {
+    const { conversationId, senderId, text } = req.body;
+    try {
+        const message = new Message({
+            conversationId,
+            sender: senderId,
+            text
+        });
+        await message.save();
 
-//         res.status(200).json(message);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// };
+        res.status(200).json(message);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
 
-// // Get messages for a conversation
-// exports.getMessages = async (req, res) => {
-//     try {
-//         const messages = await Message.find({ conversationId: req.params.conversationId });
-//         res.status(200).json(messages);
-//     } catch (err) {
-//         res.status(500).json({ error: err.message });
-//     }
-// };
+exports.getMessages = async (req, res) => {
+    try {
+        const messages = await Message.find({ conversationId: req.params.conversationId });
+        res.status(200).json(messages);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
---
+++ src/sockets/chat.socket.js
@@ -0,0 +1,71 @@
+const Message = require('../models/message.model');
+const User = require('../models/user.model');
+
+let onlineUsers = {};
+
+module.exports = (io) => {
+    io.on('connection', (socket) => {
+        console.log('User connected:', socket.id);
+
+        socket.on('join', ({ userId }) => {
+            onlineUsers[userId] = socket.id;
+            io.emit('online-users', onlineUsers); 
+        });
+
+        socket.on('send-message', async ({ conversationId, senderId, text }) => {
+            try {
+                const message = new Message({
+                    conversationId,
+                    sender: senderId,
+                    text,
+                    createdAt: new Date()
+                });
+                await message.save();
+
+                io.emit(`message-${conversationId}`, message);
+
+                if (conversationId) {
+                    const recipientId = Object.keys(onlineUsers).find(key => key !== senderId);
+                    if (recipientId) {
+                        await Message.findByIdAndUpdate(message._id, { delivered: true });
+                        io.to(onlineUsers[recipientId]).emit('message-delivered', message._id);
+                    }
+                }
+            } catch (error) {
+                console.error('Message save error:', error);
+            }
+        });
+
+        socket.on('read-message', async ({ messageId, conversationId }) => {
+            try {
+                const message = await Message.findByIdAndUpdate(
+                    messageId, 
+                    { read: true, readAt: new Date() },
+                    { new: true }
+                );
+
+                io.emit(`message-read-${conversationId}`, message);
+            } catch (error) {
+                console.error('Message read error:', error);
+            }
+        });
+
+        socket.on('disconnect', async () => {
+            console.log('User disconnected:', socket.id);
+            const userId = Object.keys(onlineUsers).find(key => onlineUsers[key] === socket.id);
+            if (userId) {
+                delete onlineUsers[userId];
+                await User.findByIdAndUpdate(userId, { lastSeen: new Date() });
+                io.emit('online-users', onlineUsers);
+            }
+        });
+
+        socket.on('typing', ({ conversationId, senderId }) => {
+            io.emit(`typing-${conversationId}`, senderId);
+        });
+
+        socket.on('stop-typing', ({ conversationId, senderId }) => {
+            io.emit(`stop-typing-${conversationId}`, senderId);
+        });
+    });
+};
